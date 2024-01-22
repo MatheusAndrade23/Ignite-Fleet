19276e98f1312710e595c141eec5ed146e352537
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2024-01-22 20:22:36 -0300

    LicensePlateInput Component

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,4 +1,5 @@
-import { Container } from "./styles";
+import { LicensePlateInput } from "../../components/LicensePlateInput";
+import { Container, Content } from "./styles";
 
 import { Header } from "../../components/Header";
 
@@ -6,6 +7,10 @@ export function Departure() {
   return (
     <Container>
       <Header title="Saída" />
+
+      <Content>
+        <LicensePlateInput label="Placa do veículo" placeholder="BRA1234" />
+      </Content>
     </Container>
   );
 }
